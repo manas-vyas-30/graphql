102f53d9c6cd7a7674d63433e6d202baf5fca1cb
manas-vyas-30 <manas.v@simformsolutions.com>
2022-09-19 18:38:38 +0530

    graphql basics

---
+++ users/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const expressGraphQL = require("express-graphql").graphqlHTTP;
+const schema = require("./schema/schema");
+
+const app = express();
+
+app.use(
+  "/graphql",
+  expressGraphQL({
+    schema,
+    graphiql: true,
+  })
+);
+
+app.listen(4000, () => {
+  console.log("Listening");
+});
+
+// http://localhost:4000/graphiql
